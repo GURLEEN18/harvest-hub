6aace9846771588e9b105550af2f4324657d49b8
chandanbir <chandanbirsingh1@gmail.com>
2024-03-02 15:01:03 +0530

    Harvest hub

--- app.py
+++ app.py
@@ -1,3 +1,25 @@
-print("hello")
-print("gurleen")
-print("harvest")
+from flask import Flask, request ,render_template as rt
+from os import getenv 
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = getenv("SECRET_KEY")
+
+@app.route('/')
+def index():
+    return rt("index.html")
+
+@app.route('/header')
+def header():
+    return rt("layout.html")
+@app.route('/aboutus')
+def aboutus():
+    return rt("aboutus.html")
+
+
+
+
+
+@app.errorhandler(404)
+def badlink(e):
+    return rt('404.html') , 404
